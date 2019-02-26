12dc66d258d3a4ec1a0cdc9f0ad519dffdbc8530
Laserzwei <lasernr2@gmail.com>
2019-02-26 19:18:43 +0100

    beta 0.21 added musiccoordinator script to init.lua (vanilla)

--- data/scripts/player/init.lua
+++ data/scripts/player/init.lua
@@ -6,5 +6,6 @@ Player():removeScript("data/scripts/player/mapcommandarrowlines.lua")
 Player():removeScript("data/scripts/player/map/mapcommandarrowlines.lua")
 
 Player():addScriptOnce("data/scripts/player/map/mapcommands.lua")
+Player():addScriptOnce("data/scripts/player/client/musiccoordinator.lua")
 
 Player():addScriptOnce("mods/SectorManager/scripts/player/sectorOpener.lua")
